9e0db8324cb42ea888fe450b3f27dc6724415562
Mohamed El adnani <mohamedeladnani0@gmail.com>
2025-01-28 16:28:01 +0100

    Add files via upload

---
+++ Mandatory/libft/ft_atoi.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-adna <mel-adna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/26 18:13:10 by mel-adna          #+#    #+#             */
+/*   Updated: 2025/01/18 18:40:22 by mel-adna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int						i;
+	int						sing;
+	unsigned long			result;
+
+	i = 0;
+	result = 0;
+	sing = 1;
+	while ((str[i] >= '\t' && str[i] <= '\r') || str[i] == ' ')
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sing = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sing);
+}
